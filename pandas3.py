2467df58d224034abf652cccda9eef89ed990d0b
zakaria <nabdo6461@gmail.com>
2025-07-07 11:48:55 +0100

    pandas

--- pandas3.py
+++ pandas3.py
@@ -1,1 +1,20 @@
 import pandas as pd
+import numpy as np
+
+data = [
+    ["Amine", 28, "Casablanca"],
+    ["Lina", 22, "Rabat"],
+    ["Youssef", 35, "Fez"],
+    ["Salma", 30, "Casablanca"],
+    ["Nora", np.nan, "Tangier"]
+]
+
+df = pd.DataFrame(data, columns=["Nom", "Âge", "Ville"])
+
+df["Année de Naissance"] = 2025 - df["Âge"]
+
+df["Nom"] = df["Nom"].str.upper()
+
+df = df.rename(columns={"Ville": "Localisation"})
+
+print(df)
---
+++ pandas4.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+data = [
+    ["Amina", 28, "casablanca"],
+    ["Lina", 22, "rabat"],
+    ["youssef", 35, "fez"],
+    ["salma", 30, "casablanca"],
+    ["nora", np.nan, "tanger"],
+]
+df = pd.DataFrame(data, columns=["Nom", "Age", "Ville"])
