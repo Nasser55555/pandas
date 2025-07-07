797444dee7dab64cea9901fe828c35fe052a8ce7
zakaria <nabdo6461@gmail.com>
2025-07-07 12:11:40 +0100

    pandas

--- pandas4.py
+++ pandas4.py
@@ -7,5 +7,13 @@ data = [
     ["salma", 30, "casablanca"],
     ["nora", np.nan, "tanger"],
 ]
-
 df = pd.DataFrame(data, columns=["Nom", "Age", "Ville"])
+df.loc[4, "Age"] = np.nan
+
+print("📍 Missing ages:")
+print(df[df["Age"].isnull()])
+
+mean_age = df["Age"].mean()
+df["Age"] = df["Age"].fillna(mean_age)
+
+print(df)
